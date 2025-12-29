1bce03ad503ea4de1a70393904d7de1711fcdfc9
adfa0107 <dlawo1822@gmail.com>
2025-12-29 19:21:57 +0900

    Revert "Revert "refactor: Change IGameEventEntity""
    This reverts commit 9360d09ed528e45f9f3ad4eb6cb6cc97d9265a5a.

--- Assets/Scripts/EventSystem/IGameEventEntity.cs
+++ Assets/Scripts/EventSystem/IGameEventEntity.cs
@@ -1,13 +1,10 @@
 using System;
-using InflationSurvivor.SkillSystem.Core;
 
 namespace InflationSurvivor.EventSystem;
 
 public interface IGameEventEntity
 {
-    public void SubscribePreEvent(GameEventType eventType, Action<SkillContext> callback);
-    public void SubscribePostEvent(GameEventType eventType, Action<SkillContext> callback);
-    public void UnsubscribePreEvent(GameEventType eventType, Action<SkillContext> callback);
-    public void UnsubscribePostEvent(GameEventType eventType, Action<SkillContext> callback);
-    public void Raise(GameEventType eventType, SkillContext eventData);
+    public void SubscribeEvent<TEventData>(Action<GameEventData> callback) where TEventData : GameEventData;
+    public void UnsubscribeEvent<TEventData>(Action<GameEventData> callback) where TEventData : GameEventData;
+    public void Raise<TEventData>(TEventData eventData) where TEventData : GameEventData;
 }
