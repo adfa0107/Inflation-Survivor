3a5203af05a2b5ff62c66324b42516cc90871435
adfa0107 <dlawo1822@gmail.com>
2025-12-29 18:59:38 +0900

    refactor: Remove Get() method

--- Assets/Scripts/Utility/ObjectPool/InstancePool.cs
+++ Assets/Scripts/Utility/ObjectPool/InstancePool.cs
@@ -1,13 +1,28 @@
+using System.Collections.Generic;
+
 namespace adfa.Utility.ObjectPool;
 
-public class InstancePool<T, TData> : SimplePool<T> where T : class, IInstance<TData>, new()
+public class InstancePool<T, TData> where T : class, IInstance<TData>, new()
 {
-    public InstancePool(int initialSize = 0) : base(initialSize) { }
+    private readonly Stack<T> _stack = new Stack<T>();
+    
+    public InstancePool(int initialSize = 0)
+    {
+        for (int i = 0; i < initialSize; i++)
+        {
+            _stack.Push(new T());
+        }
+    }
 
     public T Get(TData data)
     {
-        T instance = Get();
+        T instance = _stack.Count > 0 ? _stack.Pop() : new T();
         instance.Create(data);
         return instance;
+    }
+    
+    public void Release(T obj)
+    {
+        _stack.Push(obj);
     }
 }
