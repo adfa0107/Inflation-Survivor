e1299bf61c23f7ce466c7c557e8a936b9b622f4d
adfa0107 <dlawo1822@gmail.com>
2025-12-29 17:26:42 +0900

    refactor: changed Release method

--- Assets/Scripts/SkillSystem/Core/ConditionInstance.cs
+++ Assets/Scripts/SkillSystem/Core/ConditionInstance.cs
@@ -6,12 +6,7 @@ namespace InflationSurvivor.SkillSystem.Core;
 public abstract class ConditionInstance
 {
     public abstract void Reset();
-    public void Release()
-    {
-        ReleaseInternal();
-    }
-
-    protected abstract void ReleaseInternal();
+    public abstract void Release();
     
     public abstract bool IsActive(ISkillCaster caster);
     public abstract void Deactivate(ISkillCaster caster);
@@ -24,7 +19,7 @@ public abstract class ConditionInstance<TSelf, TData> : ConditionInstance, IInst
     private static readonly InstancePool<TSelf, TData> _pool = new InstancePool<TSelf, TData>();
     
     public static TSelf Get(TData data) => _pool.Get(data);
-    protected override void ReleaseInternal() => _pool.Release((TSelf)this);
+    public override void Release() => _pool.Release((TSelf)this);
     
     public abstract void Create(TData data);
 }
