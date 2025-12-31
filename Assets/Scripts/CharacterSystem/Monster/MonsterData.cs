c348577aecc7a8abb0b81272a37e203a70a51420
adfa0107 <dlawo1822@gmail.com>
2025-12-31 15:17:00 +0900

    fix: Change namespace deflare method

--- Assets/Scripts/CharacterSystem/Monster/MonsterData.cs
+++ Assets/Scripts/CharacterSystem/Monster/MonsterData.cs
@@ -3,11 +3,13 @@ using adfa.Utility.AI.Steering;
 using InflationSurvivor.CombatSystem.StatSystem;
 using UnityEngine;
 
-namespace InflationSurvivor.CharacterSystem.Monster;
-
-[CreateAssetMenu(menuName = "Monster")]
-public class MonsterData : StatData
+namespace InflationSurvivor.CharacterSystem.Monster
 {
-    [SerializeField] private WeightedBehavior[] behaviours;
-    public IReadOnlyList<WeightedBehavior> Behaviors => behaviours;
+    [CreateAssetMenu(menuName = "Monster")]
+    public class MonsterData : StatData
+    {
+        [SerializeField] private WeightedBehavior[] behaviours;
+        public IReadOnlyList<WeightedBehavior> Behaviors => behaviours;
+    }
 }
+
--- Assets/Scripts/SkillSystem/SkillData.cs
+++ Assets/Scripts/SkillSystem/SkillData.cs
@@ -1,12 +1,14 @@
 using InflationSurvivor.SkillSystem.Core;
 using UnityEngine;
 
-namespace InflationSurvivor.SkillSystem;
-
-[CreateAssetMenu(menuName = "Skill Card/Create Skill Card")]
-public sealed class SkillData : ScriptableObject
+namespace InflationSurvivor.SkillSystem
 {
-    [field: SerializeField] public string Name { get; private set; }
-    [field: SerializeField] public float Cooldown { get; private set; }
-    [field: SerializeField] public ComponentData[] Components { get; private set; }
+    [CreateAssetMenu(menuName = "Skill Card/Create Skill Card")]
+    public sealed class SkillData : ScriptableObject
+    {
+        [field: SerializeField] public string Name { get; private set; }
+        [field: SerializeField] public float Cooldown { get; private set; }
+        [field: SerializeField] public ComponentData[] Components { get; private set; }
+    }
 }
+
