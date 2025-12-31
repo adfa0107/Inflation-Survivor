e372e736677bb44c311fe68b79b7eaa5aafa01bb
adfa0107 <dlawo1822@gmail.com>
2025-12-31 15:19:49 +0900

    feat: add max cost stat

--- Assets/Scripts/CombatSystem/StatSystem/Stat.cs
+++ Assets/Scripts/CombatSystem/StatSystem/Stat.cs
@@ -1,14 +1,22 @@
 using System.Collections.Generic;
+using InflationSurvivor.CombatSystem.ResourceSystem;
 
 namespace InflationSurvivor.CombatSystem.StatSystem;
 
 public class Stat
 {
     private readonly Dictionary<StatType, float> _stat = new Dictionary<StatType, float>();
+    private readonly Dictionary<CostType, float> _maxCost = new Dictionary<CostType, float>();
         
     public float this[StatType type]
     {
         get => _stat.GetValueOrDefault(type, 0f);
         set => _stat[type] = value;
+    }
+
+    public float this[CostType type]
+    {
+        get => _maxCost.GetValueOrDefault(type, 0f);
+        set => _maxCost[type] = value;
     }
 }
