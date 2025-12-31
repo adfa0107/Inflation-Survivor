b32b1bdf474b7e2ee1e7c6a85098a52185bb9732
adfa0107 <dlawo1822@gmail.com>
2025-12-31 17:49:34 +0900

    feat: Add Raise method

--- Assets/Scripts/EventSystem/GameEvent.cs
+++ Assets/Scripts/EventSystem/GameEvent.cs
@@ -2,5 +2,19 @@ namespace InflationSurvivor.EventSystem;
 
 public abstract class GameEvent
 {
-    
+    public static (bool isCancelled, TEvent result) RaisePrev<TEvent>(TEvent @event) where TEvent : struct, IEvent
+    {
+        Prev<TEvent> prevEvent = Prev<TEvent>.Get(@event);
+        @event.RaiseToTarget(prevEvent);
+        (bool, TEvent) result = (prevEvent.isCancelled, prevEvent.data);
+        prevEvent.Release();
+        return result;
+    }
+
+    public static void RaisePost<TEvent>(TEvent @event) where TEvent : struct, IEvent
+    {
+        Post<TEvent> postEvent = Post<TEvent>.Get(@event);
+        @event.RaiseToTarget(postEvent);
+        postEvent.Release();
+    }
 }
