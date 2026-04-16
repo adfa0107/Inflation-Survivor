82e6c3d9c1dd98cee6922b81cccd137607e49797
adfa0107 <dlawo1822@gmail.com>
2026-04-16 13:31:25 +0900

    feat: Add Serializable to NoneDefinition

--- Assets/Scripts/Skills/DirectionSelectors/NoneDefinition.cs
+++ Assets/Scripts/Skills/DirectionSelectors/NoneDefinition.cs
@@ -1,7 +1,9 @@
+using System;
 using InflationSurvivor.Skills.Primitives;
 
 namespace InflationSurvivor.Skills.DirectionSelectors;
 
+[Serializable]
 public class NoneDefinition : DirectionSelectorDefinition
 {
     public override DirectionSelector Compile()
