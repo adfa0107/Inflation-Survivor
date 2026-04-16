1ab30ce358762651080a1f72733f821758ae8d2e
adfa0107 <dlawo1822@gmail.com>
2026-04-16 13:17:29 +0900

    feat: Implement PreDefinedDefinition

--- Assets/Scripts/Skills/TargetSources/PreDefinedDefinition.cs
+++ Assets/Scripts/Skills/TargetSources/PreDefinedDefinition.cs
@@ -1,6 +1,13 @@
+using InflationSurvivor.CombatSystem;
+using InflationSurvivor.Skills.Primitives;
+using InflationSurvivor.Skills.Primitives.Targets;
+
 namespace InflationSurvivor.Skills.TargetSources;
 
-public class PreDefinedDefinition
+public class PreDefinedDefinition : TargetSourceDefinition
 {
-    
+    public override TargetSource Compile(ISkillProcessor<CombatModule> processor)
+    {
+        return new PreDefined(processor);
+    }
 }
